eac84a26dd875b34cdfca1f15148d7b095c0db20
Asif R Choudhury <choudhury.asif@gmail.com>
2017-07-02 15:52:15 -0500

    Return blot image by id

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var inkblots = require('./routes/inkblots');
 
 var app = express();
 
@@ -19,6 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/inkblots', inkblots);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/inkblots.js
@@ -0,0 +1,28 @@
+var router = require('express').Router();
+var fs = require('fs')
+
+
+var showInkblot = function(req, res, id) {
+  id = id || 1;
+  var blotId = ("00" + id).slice(-2);
+  var fileName = "inkblots/Rorschach_blot_" + blotId + ".jpg";
+  fs.readFile(fileName, function(err, data) {
+    if (err) {
+      console.warn(err);
+      var status =  (err.code == "ENOENT") ? 404 : 500;
+      res.locals.message = "Not Found";
+      res.locals.error = {};
+      res.status(status);
+      res.render('error');
+      return;
+    }
+    res.writeHead(200, {'Content-Type': "image/jpeg"});
+    res.end(data);
+  });
+};
+
+router.get('/blot/:id', function(req, res) {
+  showInkblot(req, res, req.params.id);
+});
+
+module.exports = router;
